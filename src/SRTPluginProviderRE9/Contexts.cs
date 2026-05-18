aa849aae372f77f763a6d789cb4db2763898339f
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2026-05-17 21:12:05 -0500

    Adjust C# SRT plugin to have proper CharacterContext ID and KindID values finally.

--- src/SRTPluginProviderRE9/Contexts.cs
+++ src/SRTPluginProviderRE9/Contexts.cs
@@ -4,12 +4,17 @@ using SRTPluginProviderRE9.Extensions;
 
 namespace SRTPluginProviderRE9
 {
-    public abstract record class CharacterContext
+    public abstract unsafe record class CharacterContext
     {
         public CharacterContext(ProcessMemoryHandler processMemoryHandler, MultilevelPointer multilevelPointer)
         {
-            ID = multilevelPointer.Deref<Guid>(0x38);
-            KindID = multilevelPointer.DerefUShort(0x40);
+            var contextIdPtr = processMemoryHandler.DerefChain<nuint>(multilevelPointer.Address, 0x38);
+            ID = processMemoryHandler.GetAt<Guid>(contextIdPtr + 0x10U);
+
+            var kindIdPtr = processMemoryHandler.DerefChain<nuint>(multilevelPointer.Address, 0x40, 0x10);
+            var kindIdLength = processMemoryHandler.GetAt<int>(kindIdPtr + 0x10U) * sizeof(char);
+            KindID = processMemoryHandler.GetUnicodeStringAt(UIntPtr.Add(kindIdPtr, 0x14).ToPointer(), (nuint)kindIdLength);
+
             HP = new HPData
             {
                 CurrentHP = processMemoryHandler.DerefChain<int>(multilevelPointer.Address, 0x70, 0x10, 0x28),
@@ -24,8 +29,8 @@ namespace SRTPluginProviderRE9
             ShowVitalSkipFrame = multilevelPointer.DerefByte(0xF2) != 0;
         }
 
-        public Guid ID { get; internal set; } // 0x38
-        public ushort KindID { get; internal set; } // 0x40
+        public Guid ID { get; internal set; } // 0x38 -> 0x16
+        public string KindID { get; internal set; } // 0x40 -> 0x10 -> 0x14 + N
         public HPData HP { get; internal set; } // 0x70 -> 0x10 -> (0x28, 0x30)
         public bool IsRespawn { get; internal set; } // 0xEC
         public bool IsSpawn { get; internal set; } // 0xED
